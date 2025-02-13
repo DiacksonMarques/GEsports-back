0799b946825edc42bbd1d97b1be99feea84793b4
Diackson Marques <diackson123@gmail.com>
2025-02-13 15:55:23 -0300

    Ajustes de presença

--- app/Controllers/SelectiveController.php
+++ app/Controllers/SelectiveController.php
@@ -205,19 +205,27 @@ class SelectiveController extends ResourceController{
       array_shift($candidates);
 
       foreach($candidates as &$candidate){
-        $modelEdi = new EfiPayModel();
-        $responsePix = $modelEdi->searchPix($candidate->txid);
-
-        if($responsePix['status'] != 201){
-          return $this->fail($responsePix);
-        }
-
-        if($responsePix['body']['status'] == "CONCLUIDA"){
+        if(property_exists($candidate, 'pixStatus') && $candidate->pixStatus == "CONCLUIDA"){
           $response[] = $candidate;
+        } else if (!property_exists($candidate, 'pixStatus')){
+          $modelEdi = new EfiPayModel();
+          $responsePix = $modelEdi->searchPix($candidate->txid);
+
+          if($responsePix['status'] != 201){
+            return $this->fail($responsePix);
+          }
+
+          $candidate->pixStatus = $responsePix['body']['status'];
+
+          if($responsePix['body']['status'] == "CONCLUIDA"){
+            $response[] = $candidate;
+          }
         }
+
       }
 
-      return $this->respond($candidates);
+      $this->saveSelective($candidates);
+      return $this->respond($response);
     }
 
     public function getCandidateForEvaluation($hour=null, $gender=null){
